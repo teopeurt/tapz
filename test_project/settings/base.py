4fd1172ea04deef067c0d737fdb0814f5532c5c7
Honza Kral <honza.kral@gmail.com>
2010-08-14 15:40:26 -0700

    Typo

--- test_project/settings/base.py
+++ test_project/settings/base.py
@@ -22,7 +22,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'tapz.error_panel.middleware.ErrorPanelMIddleware',
+    'tapz.error_panel.middleware.ErrorPanelMiddleware',
 )
 
 TEMPLATE_DIRS = (
