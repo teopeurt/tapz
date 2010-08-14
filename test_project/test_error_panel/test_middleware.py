c353814cb277ea59c016d34f2aae44a7f6db8724
Honza Kral <honza.kral@gmail.com>
2010-08-14 16:27:52 -0700

    Test veryfying that error catching middleware reports exceptions correctly

--- test_project/test_error_panel/test_middleware.py
+++ test_project/test_error_panel/test_middleware.py
@@ -1,9 +1,21 @@
+import anyjson
+
 from django.core.urlresolvers import reverse
 
 from test_project.helpers import DjangoRedisTestCase
+from test_project import test_template_loader
 
 class TestErrorPanelMiddleware(DjangoRedisTestCase):
     def test_status_500_is_returned(self):
+        test_template_loader.templates['500.html'] = '500'
         resp = self.client.get(reverse('test-fail-horribly'))
         self.assertEquals(500, resp.status_code)
+
+    def test_correct_data_is_stpored_in_redis(self):
+        test_template_loader.templates['500.html'] = '500'
+        resp = self.client.get(reverse('test-fail-horribly'))
+
+        exc_data = anyjson.deserialize(self.redis.get('errors:1'))
+        self.assertEquals(11, exc_data['line_number'])
+        self.assertEquals('MyException', exc_data['exc_name'])
 
