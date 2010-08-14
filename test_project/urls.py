d6561b98884df1dd9538c10b98f4e07a59c74dbe
Honza Kral <honza.kral@gmail.com>
2010-08-14 15:41:18 -0700

    Added a failing view

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls.defaults import patterns, url 
+from django.conf.urls.defaults import patterns, url, handler500
 
 from test_project import test_views
 
 urlpatterns = patterns('',
-    url(r'', test_views.throw_exception, name='test-throw-exception'),
+    url(r'fail/$', test_views.fail_horribly, name='test-fail-horribly'),
 )
