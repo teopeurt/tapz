9910bbfcefb1d372bf7ffc0edd6ba1c7b5b0a9b8
Honza Kral <honza.kral@gmail.com>
2010-08-14 16:02:05 -0700

    Added panel autodiscovery

--- tapz/site.py
+++ tapz/site.py
@@ -1,5 +1,33 @@
 class TapzSite(object):
     _panels = {}
+    _initialized = False
+
+    def _auto_discover(self):
+        """
+        Auto-discover INSTALLED_APPS panels.py modules and fail silently when
+        not present. This forces an import on them to register any panels they
+        define. 
+
+        Copied from django/contrib/admin/__init__.py, Thanks!
+        """
+    
+        from django.conf import settings
+        from django.utils.importlib import import_module
+        from django.utils.module_loading import module_has_submodule
+    
+        self._initialized = True
+        for app in settings.INSTALLED_APPS:
+            mod = import_module(app)
+            # Attempt to import the app's panels module.
+            try:
+                import_module('%s.panels' % app)
+            except:
+                # Decide whether to bubble up this error. If the app just
+                # doesn't have an panels module, we can ignore the error
+                # attempting to import it, otherwise we want it to bubble up.
+                if module_has_submodule(mod, 'panels'):
+                    raise
+
 
     def register(self, panel):
         """
@@ -18,6 +46,8 @@ class TapzSite(object):
         """
         Retrieve a registered panel instance by the event type it handles.
         """
+        if not self._initialized:
+            self._auto_discover()
         if event_type in self.__class__._panels:
             return self.__class__._panels[event_type]
         raise Exception("No panel that handles event type '%s' found" % event_type)
