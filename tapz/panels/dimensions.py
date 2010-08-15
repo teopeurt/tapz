93d145df254817f5d391444f3b822c43a5b259bf
andy <andy@whiskeymedia.com>
2010-08-14 17:14:37 -0700

    Cleaning up the DateTimeDimension

--- tapz/panels/dimensions.py
+++ tapz/panels/dimensions.py
@@ -1,12 +1,14 @@
+import datetime
 from django.contrib.sites.models import Site
 
 class Dimension(object):
+    "Base class for a Dimension"
     def __init__(self):
         self.name = None
 
     def contribute_to_class(self, cls, name):
-        cls._meta.dimensions[name] = self
         self.name = name
+        cls._meta.dimensions[name] = self
 
     def get_display(self, value):
         return value
@@ -14,21 +16,60 @@ class Dimension(object):
     def split(self, value):
         return [value]
 
-# NOTE: order is important here --
+class Interval(object):
+    "Base date interval class"
+    @classmethod
+    def range(cls, start, end):
+        """
+        Return a range of datetime objects between start and end
+        """
+        r = []
+        while start <= end:
+            r.append(start)
+            start += cls.delta
+        return r
+
+    @classmethod
+    def format(cls, value):
+        """
+        Return a datetime object representing the timestamp value
+        """
+        return datetime.datetime.strftime(value, cls.format_string)
+
+class Month(Interval):
+    format_string = '%Y%m'
+
+    def range(self, start, end):
+        # there's no "months" arg for timedelta.
+        r = []
+        # reset the start date to the beginning of the month
+        start = datetime.datetime(year=start.year, month=start.month, day=1)
+        while start <= end:
+            r.append(start)
+            if start.month == 12:
+                start = datetime.datetime(year=start.year+1, month=1, day=1)
+            else:
+                start = datetime.datetime(year=start.year, month=start.month+1, day=1)
+        return r
+
+class Day(Interval):
+    format_string = '%Y%m%d'
+    delta = datetime.timedelta(days=1)
+
+class Hour(Interval):
+    format_string = '%Y%m%d%H'
+    delta = datetime.timedelta(hours=1)
+
+# NOTE: order is important here
 # we really want least granular to most granular
-DEFAULT_DATETIME_FORMATS = (
-    '%Y%m', #month
-    '%Y%m%d', #day
-    '%Y%m%d%H', #hour
-    )
+DEFAULT_DATETIME_FORMATS = (Month, Day, Hour)
 
 class DateTimeDimension(Dimension):
     def __init__(self, datetime_formats=DEFAULT_DATETIME_FORMATS):
         self.datetime_formats = datetime_formats
 
-    def get_display(self, value):
-        # TODO: use length of value to format it into a datetime object
-        return value
+    def get_display(self, value, interval):
+        return interval.format(value)
 
     def split(self, value):
         return [value.strftime(fmt) for fmt in self.datetime_formats]
