d2fac5f4b77ad7a71a6cfddf93569f3372a1c8e4
andy <andy@whiskeymedia.com>
2010-08-15 20:53:45 -0700

    1 hour left! this panel ain't getting done.

--- tapz/pagespeed/panels.py
+++ tapz/pagespeed/panels.py
@@ -1,10 +1,14 @@
+import random
 from django.views.generic.simple import direct_to_template
 from tapz import panels
 
 class PageSpeedPanel(panels.Panel):
+    GOOD = 100
+    NORMAL = 500
+
     class Meta:
         title = 'Page Speed'
         event_type = 'pagespeed'
 
-    def call_index(self, request, context):
+    def call_index(self, request, context, **filters):
         return direct_to_template(request, 'pagespeed/index.html', context)
